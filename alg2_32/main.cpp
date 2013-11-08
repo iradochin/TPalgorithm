787032e6b376fb0d48fc73fb4dd19ad3d988d61f
deyl94 <radochin_ilya@list.ru>
2013-11-08 17:23:27 +0400

    delete whitespaces

--- alg2_32/main.cpp
+++ alg2_32/main.cpp
@@ -1,31 +1,31 @@
 /*
- *        Name: main.cpp
- *  Created on: Nov 4, 2013
- *      Author: roland
- *	 Copiright: 2013 All rights reserved
- */
+* Name: main.cpp
+* Created on: Nov 4, 2013
+* Author: roland
+*         Copiright: 2013 All rights reserved
+*/
 
 /*
- * Даны неотрицательные целые числа n,k и массив целых сичел из [0..10^9]
- * размера n. Требуется найти k-ю порядковую статистику. т.е.
- * напечатать число, которое бы стояло на позиции с индексом k (0..n-1)
- * в отсортированном массиве. Напишите нерекурсивный алгоритм.
- * Требования к дополнительной памяти: O(n).
- * Требуемое среднее время работы: O(n).
- *
- * 3.2
- * Реализуйте стратегию выбора опорного элемента “медиана трёх”.
- * Функцию Partition реализуйте методом прохода двумя итераторами
- * от конца массива к началу.
- */
+* Даны неотрицательные целые числа n,k и массив целых сичел из [0..10^9]
+* размера n. Требуется найти k-ю порядковую статистику. т.е.
+* напечатать число, которое бы стояло на позиции с индексом k (0..n-1)
+* в отсортированном массиве. Напишите нерекурсивный алгоритм.
+* Требования к дополнительной памяти: O(n).
+* Требуемое среднее время работы: O(n).
+*
+* 3.2
+* Реализуйте стратегию выбора опорного элемента “медиана трёх”.
+* Функцию Partition реализуйте методом прохода двумя итераторами
+* от конца массива к началу.
+*/
 
 #include <iostream>
 #include <vector>
 //#include <algorithm>
 
-void medianOfThree (std::vector<int>&, int, int ); // Установка медианы в нужное место
-int partition ( std::vector <int> &, int, int ); // Нахождение места элемента
-int kStat ( std::vector<int>&, int, int, int ); //к-я статистика
+void medianOfThree (std::vector<int>&, int, int); // Установка медианы в нужное место
+int partition (std::vector <int> &, int, int); // Нахождение места элемента
+int kStat (std::vector<int>&, int, int, int); //к-я статистика
 
 int main ()
 {
@@ -33,16 +33,16 @@ int main ()
     std::cin >> n;
     int k = 0;
     std::cin >> k;
-    std::vector<int> a( n, 0 );
-    for ( int i = 0; i < n ; ++i )
+    std::vector<int> a(n, 0);
+    for (int i = 0; i < n ; ++i)
     {
         std::cin >> a[i];
     }
 
-    std::cout << kStat ( a, 0, n - 1, k );
+    std::cout << kStat (a, 0, n - 1, k);
 
-//    nth_element ( a.begin(), a.begin() + k, a.end() );
-//    std::cout << a[k];
+// nth_element ( a.begin(), a.begin() + k, a.end() );
+// std::cout << a[k];
 }
 
 void medianOfThree (std::vector<int>& a, int left, int right)
@@ -52,15 +52,15 @@ void medianOfThree (std::vector<int>& a, int left, int right)
     // [left, ( left + right ) / 2, right]
     // т.е. на месте a[left] становится средний из 3х элементов
 
-    if ( a[( left + right ) / 2] > a[right] )
-        std::swap ( a[( left + right ) / 2], a[right] );
-    if ( a[left] < a[( left + right ) / 2] )
-        std::swap ( a[left], a[( left + right ) / 2] );
-    if ( a[left] > a[right] )
-        std::swap ( a[left], a[right] );
+    if (a[( left + right ) / 2] > a[right])
+        std::swap (a[( left + right ) / 2], a[right]);
+    if (a[left] < a[( left + right ) / 2])
+        std::swap ( a[left], a[( left + right ) / 2]);
+    if (a[left] > a[right])
+        std::swap (a[left], a[right]);
 }
 
-int partition ( std::vector <int> &a, int left, int right )
+int partition (std::vector <int> &a, int left, int right)
 {
     // Метод прохода двумя итераторами от конца массива к началу.
     medianOfThree( a, left, right );
@@ -68,30 +68,30 @@ int partition ( std::vector <int> &a, int left, int right )
     int j = right;
     bool step = false;
 
-    while ( j > left )
+    while (j > left)
     {
         // Ускорение для повторяющихся элементов
-        while ( a[left] == a[j] && j > left )
+        while (a[left] == a[j] && j > left)
         {
-            if ( step )
+            if (step)
                 std::swap(a[i--], a[j--]);
             else
                 --j;
             step=!step;
         }
 
-        while ( a[j] < a[left] && j > left )
+        while (a[j] < a[left] && j > left)
             --j;
 
-        while ( a[j] > a[left] && j > left )
-            std::swap( a[j--], a[i--] );
+        while (a[j] > a[left] && j > left)
+            std::swap(a[j--], a[i--]);
     }
-    std::swap ( a[left], a[i] );
+    std::swap (a[left], a[i]);
 
     return i;
 }
 
-int kStat ( std::vector<int>& a, int left, int right, int k )
+int kStat (std::vector<int>& a, int left, int right, int k)
 {
     // Модификация quick_sort для поиска к-й статистики
     int l = left;
@@ -99,8 +99,8 @@ int kStat ( std::vector<int>& a, int left, int right, int k )
     int p = -1;
     while ( p != k )
     {
-        p = partition ( a, l, r );
-        if ( p < k )
+        p = partition (a, l, r);
+        if (p < k)
             l = p + 1;
         else
             r = p - 1;
