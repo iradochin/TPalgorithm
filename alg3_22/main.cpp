956da1de20b1b3ce2fc490ff147d7a2cd211f9fe
deyl94 <radochin_ilya@list.ru>
2013-11-18 03:27:25 +0400

    some refactoring

--- alg3_22/main.cpp
+++ alg3_22/main.cpp
@@ -44,145 +44,12 @@ struct TreapNode
     };
 
 typedef TreapNode* pTreapNode;
-void split (pTreapNode in, int data, pTreapNode& l, pTreapNode& r)
-{
-    // Разделяет дерево in на два дерева l и r,
-    // таким образом, что l содержит все элементы, меньшие data,
-    // а r содержит все элементы, большие или равные data.
-
-    if (in == NULL)
-    {
-        // Если нашли точку разделения
-
-        l = NULL;
-        r = NULL;
-    }
-    else
-        if (data < in->data)
-        {
-            // Определяем правое дерево r.
-
-            split(in->left, data, l, in->left);
-            r = in;
-        }
-        else
-        {
-            // Определяем левое дерево l.
-
-            split (in->right, data, in->right, r);
-            l = in;
-        }
-}
-
-void insertDecTree (pTreapNode& root, pTreapNode in)
-{
-    // Функция вставляет элемент in в декартово дерево с корнем root
-
-    if (root == NULL)
-    {
-        // Если дошли до конца дерева
-
-        root = in;
-    }
-    else
-        if (in->priority > root->priority)
-        {
-            // Если нашли место - вызываем Split() от найденного элемента
-            // (от элемента вместе со всем его поддеревом), и возвращаемые
-            // им l и r деревья записываем в качестве левого и правого
-            // сына добавляемого элемента.
-
-            split (root, in->data, in->left, in->right);
-            root = in;
-        }
-        else
-            // Cпускаемся по дереву, и останавливаемся
-            // на первом элементе, у которого значение приоритета
-            // оказалось меньше in->priority.
-            if (in->data < root->data)
-            {
-                insertDecTree(root->left, in);
-            }
-            else
-            {
-                insertDecTree(root->right, in);
-            }
-}
-
-void insertBinaryTree (pTreapNode& in, int data)
-{
-    // Рекурсивная вставка элемента в бинарное дерево заданное
-    // наивным порядком вставки.
-
-    if (in == NULL)
-    {
-        in = new TreapNode(data);
-        return;
-    }
-
-    // Если новый ключ >= ,то помещаем его в правое поддерево, иначе в левое
-    if (data >= in->data)
-        insertBinaryTree(in->right, data);
-    else
-        insertBinaryTree(in->left, data);
-}
-
-size_t getHeight(TreapNode *root)
-{
-    // Функция возвращает количество слоев в дереве
-
-        if (root == NULL)
-            // Если дерево пустое
-            return 0;
-
-        // Ищем глубину правого и левого поддерева и выбыраем максимальную
-        // глубину + 1 (корень).
-
-        size_t hLeft = 0, hRight = 0;
-        if (root->left != NULL)
-            hLeft = getHeight(root->left);
-
-        if (root->right)
-            hRight = getHeight(root->right);
-
-        return std::max(hLeft, hRight) + 1;
-}
-
-size_t getWidth (TreapNode* in, size_t level)
-{
-    // Функция возвращает ширину слоя level дерева (слои начиная с нуля).
-
-    if (in == NULL)
-        // Если дерево пустое.
-        return 0;
-
-    if (level == 0)
-        // Выход из рекурсии, если достигли необходимого слоя.
-        return 1;
-
-    if(level > 0)
-        return getWidth(in->left, level - 1) + getWidth(in->right, level - 1);
-
-    return 0;
-}
-
-int getMaxWidth (TreapNode* root)
-{
-    // Функция возвращает максимальную ширину среди всех слоев дерева.
-
-    size_t height = getHeight(root);
-    size_t maxWidth = 0;
-    for (size_t i = 0; i < height; ++i)
-    {
-        // Для каждого слоя ищем его ширину.
-
-        size_t width = getWidth(root, i);
-        if (maxWidth < width)
-            maxWidth = width;
-    }
-
-    return maxWidth;
-}
+void split (pTreapNode,const int, pTreapNode&, pTreapNode&);
+void insertDecTree (pTreapNode&,const pTreapNode);
+void insertBinaryTree (pTreapNode&,const int);
+size_t getHeight(const TreapNode*);
+size_t getWidth (const TreapNode*, const size_t);
+int getMaxWidth (const TreapNode*);
 
 int main()
 {
@@ -222,4 +89,144 @@ int main()
     std::cout << two - one;
 
     return 0;
+}
+
+void split (pTreapNode in, const int data, pTreapNode& l, pTreapNode& r)
+{
+    // Разделяет дерево in на два дерева l и r,
+    // таким образом, что l содержит все элементы, меньшие data,
+    // а r содержит все элементы, большие или равные data.
+
+    if (in == NULL)
+    {
+        // Если нашли точку разделения
+
+        l = NULL;
+        r = NULL;
+    }
+    else
+        if (data < in->data)
+        {
+            // Определяем правое дерево r.
+
+            split(in->left, data, l, in->left);
+            r = in;
+        }
+        else
+        {
+            // Определяем левое дерево l.
+
+            split (in->right, data, in->right, r);
+            l = in;
+        }
+}
+
+void insertDecTree (pTreapNode& root, const pTreapNode in)
+{
+    // Функция вставляет элемент in в декартово дерево с корнем root
+
+    if (root == NULL)
+    {
+        // Если дошли до конца дерева
+
+        root = in;
+    }
+    else
+        if (in->priority > root->priority)
+        {
+            // Если нашли место - вызываем Split() от найденного элемента
+            // (от элемента вместе со всем его поддеревом), и возвращаемые
+            // им l и r деревья записываем в качестве левого и правого
+            // сына добавляемого элемента.
+
+            split (root, in->data, in->left, in->right);
+            root = in;
+        }
+        else
+            // Cпускаемся по дереву, и останавливаемся
+            // на первом элементе, у которого значение приоритета
+            // оказалось меньше in->priority.
+            if (in->data < root->data)
+            {
+                insertDecTree(root->left, in);
+            }
+            else
+            {
+                insertDecTree(root->right, in);
+            }
+}
+
+void insertBinaryTree (pTreapNode& in, const int data)
+{
+    // Рекурсивная вставка элемента в бинарное дерево заданное
+    // наивным порядком вставки.
+
+    if (in == NULL)
+    {
+        in = new TreapNode(data);
+        return;
+    }
+
+    // Если новый ключ >= ,то помещаем его в правое поддерево, иначе в левое
+    if (data >= in->data)
+        insertBinaryTree(in->right, data);
+    else
+        insertBinaryTree(in->left, data);
+}
+
+size_t getHeight(const TreapNode *root)
+{
+    // Функция возвращает количество слоев в дереве
+
+        if (root == NULL)
+            // Если дерево пустое
+            return 0;
+
+        // Ищем глубину правого и левого поддерева и выбыраем максимальную
+        // глубину + 1 (корень).
+
+        size_t hLeft = 0, hRight = 0;
+        if (root->left != NULL)
+            hLeft = getHeight(root->left);
+
+        if (root->right)
+            hRight = getHeight(root->right);
+
+        return std::max(hLeft, hRight) + 1;
+}
+
+size_t getWidth (const TreapNode* in, const size_t level)
+{
+    // Функция возвращает ширину слоя level дерева (слои начиная с нуля).
+
+    if (in == NULL)
+        // Если дерево пустое.
+        return 0;
+
+    if (level == 0)
+        // Выход из рекурсии, если достигли необходимого слоя.
+        return 1;
+
+    if(level > 0)
+        return getWidth(in->left, level - 1) + getWidth(in->right, level - 1);
+
+    return 0;
+}
+
+int getMaxWidth (const TreapNode* root)
+{
+    // Функция возвращает максимальную ширину среди всех слоев дерева.
+
+    size_t height = getHeight(root);
+    size_t maxWidth = 0;
+    for (size_t i = 0; i < height; ++i)
+    {
+        // Для каждого слоя ищем его ширину.
+
+        size_t width = getWidth(root, i);
+        if (maxWidth < width)
+            maxWidth = width;
+    }
+
+    return maxWidth;
 }
